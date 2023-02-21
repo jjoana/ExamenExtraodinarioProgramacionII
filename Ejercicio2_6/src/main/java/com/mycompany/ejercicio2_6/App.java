874c21d6b9f08aacf5957a56dc7eda5d014001e1
jjoana <jesus.joanaazuara@usp.ceu.es>
2023-02-21 19:32:11 +0100

    Ejercicio de desviacion tipica

--- Ejercicio2_6/src/main/java/com/mycompany/ejercicio2_6/App.java
+++ Ejercicio2_6/src/main/java/com/mycompany/ejercicio2_6/App.java
@@ -4,15 +4,26 @@ import java.util.ArrayList;
 
 public class App {
 
+
+    //Creacion del main
     public static void main(String[] args) {
-        ArrayList<Double> lista = new ArrayList<>();
+        
+        //Creacion del ArrayList
+        ArrayList<Double> lista = new ArrayList<Double>();
         int tamano;
+        //Creacion de la lista
         lista.add(1.0);
         lista.add(2.0);
+        lista.add(3.0);
+        lista.add(4.0);
+        lista.add(5.0);
+        lista.add(6.0);
 
+        
         tamano = lista.size();
         DesviacionTipica calculo = new DesviacionTipica(lista);
         double mediaAritmetica = calculo.calculoMediaAritmeticaR(tamano);
+        System.out.println(mediaAritmetica);
         double desviacionTipica = calculo.calcularDesviacionTipicaR(tamano, mediaAritmetica);
         System.out.println(desviacionTipica);
     }
--- Ejercicio2_6/src/main/java/com/mycompany/ejercicio2_6/DesviacionTipica.java
+++ Ejercicio2_6/src/main/java/com/mycompany/ejercicio2_6/DesviacionTipica.java
@@ -13,21 +13,38 @@ import java.util.List;
  */
 public class DesviacionTipica {
 
+
+    //Poner un ArrayList en private sirve para que solo los métodos de la clase puedan acceder y modificar el ArrayList.
     int tamanoLista = 0;
     private ArrayList<Double> listado;
     private ArrayList<Double> copiaListaCalculomedia;
     private ArrayList<Double> copiaListaDesviacionTipica;
 
     public DesviacionTipica(ArrayList<Double> listado) {
+
+        //Copia de la lista para no modificar el listado.
         copiaListaCalculomedia = (ArrayList<Double>) listado.clone();
+
+        //Copia del listado para no modificar el listado
         copiaListaDesviacionTipica = (ArrayList<Double>) listado.clone();
+
+        //Guardo el tamaño del listado
         this.listado = listado;
         tamanoLista = listado.size();
     }
 
     public double calculoMediaAritmeticaR(int tamano) {
+
+        //Se selecciona el primer elemeto de CopiaListaCalculoMedia y se divide entre su tamaño
         double valor = (double) copiaListaCalculomedia.get(0) / tamanoLista;
+
+        //Prueba para ver que se están eliminando los elementos
         copiaListaCalculomedia.remove(0);
+
+        //Prueba que hago para ver si eleminan todos los elementos de la lista recursivamente
+        System.out.println("Eliminacion");
+
+        //si el tamaño es mayor que 1, se llama recursivamente a la función con el tamaño disminuido en 1, y al resultado se le suma el valor actual.
         if (tamano > 1) {
             valor += calculoMediaAritmeticaR(tamano - 1);
         }
@@ -35,9 +52,17 @@ public class DesviacionTipica {
     }
 
     public double calcularDesviacionTipicaR(int tamano, double mediaAritmetica) {
+
+        //calcula la diferencia que hay entre el primer elemento de copiaListaDesviacionTipica y la media aritmética de los numeros
         double base = (double) (copiaListaDesviacionTipica.get(0) - mediaAritmetica);
+
+        //utiliza la fórmula de la varianza para calcular el conjunto de números
         double valor = (double)(Math.pow(base,2)/(tamanoLista - 1));
+
+        //elimina el primer elemento de la lista copiaListaDesviacionTipica
         copiaListaDesviacionTipica.remove(0);
+
+        //si el tamaño es mayor que 1, se llama recursivamente a la función con el tamaño disminuido en 1, y al resultado se le suma el valor actual.
         if (tamano > 1) {
             valor += calcularDesviacionTipicaR(tamano - 1 , mediaAritmetica);
         }
